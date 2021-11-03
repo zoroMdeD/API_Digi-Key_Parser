98be983b0ce649f4760c04a3524e563a49cb6c17
MaximM <Morozov.M.biz@yandex.ru>
2021-11-04 00:34:22 +0300

    Add_on_GUI_toolstrip

--- API_Digi-Key_Parser_new/Form1.Designer.cs
+++ API_Digi-Key_Parser_new/Form1.Designer.cs
@@ -33,13 +33,15 @@ namespace API_Digi_Key_Parser_new
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(296, 121);
+            this.button1.Location = new System.Drawing.Point(395, 149);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,19 +49,21 @@ namespace API_Digi_Key_Parser_new
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 156);
+            this.textBox1.Location = new System.Drawing.Point(16, 192);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(440, 140);
+            this.textBox1.Size = new System.Drawing.Size(585, 171);
             this.textBox1.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(377, 121);
+            this.button2.Location = new System.Drawing.Point(503, 149);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 2;
             this.button2.Text = "OAuth";
             this.button2.UseVisualStyleBackColor = true;
@@ -67,22 +71,34 @@ namespace API_Digi_Key_Parser_new
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 302);
+            this.progressBar1.Location = new System.Drawing.Point(16, 372);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(440, 8);
+            this.progressBar1.Size = new System.Drawing.Size(587, 10);
             this.progressBar1.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(616, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 322);
+            this.ClientSize = new System.Drawing.Size(616, 385);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.MaximumSize = new System.Drawing.Size(480, 360);
-            this.MinimumSize = new System.Drawing.Size(480, 360);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(634, 432);
+            this.MinimumSize = new System.Drawing.Size(634, 432);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -98,6 +114,7 @@ namespace API_Digi_Key_Parser_new
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
     }
 }
 
